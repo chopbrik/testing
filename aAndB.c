7253993d869d47861eb5ce41cb05a3427a7c83a5
chopbrik <orangewood95@live.com>
2018-10-09 19:33:46 -0400

    Now it will also "know" Alice

--- aAndB.c
+++ aAndB.c
@@ -6,11 +6,11 @@ int main(void)
 	char name[100];
 	printf("Enter your name: \n");
 	scanf("%s", name);
-	if (strcmp(name, "Bob") == 0)
+	if (strcmp(name, "Bob") == 0 || !strcmp(name, "Alice"))
 	{
 		printf("%s\n", name);
 		return 0;
 	}
-	printf("Oh, I don't know you");
+	printf("Oh, I don't know you\n");
 	return 0;
 }
